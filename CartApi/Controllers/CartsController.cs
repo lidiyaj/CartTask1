1433b43442b264fd3f2e62429a6b64a08ae8f43b
Lidiya Joseph <lidiyajoseph@gmail.com>
2022-02-28 16:47:20 +0800

    Modified

---
+++ CartApi/Controllers/CartsController.cs
@@ -0,0 +1,160 @@
+﻿using CartApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly CartDbContext _context;
+
+        public OrderController(CartDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Carts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetCarts()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Carts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetCart(int id)
+        {
+            var cart = await _context.Orders.FindAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return cart;
+        }
+
+        // PUT: api/Carts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCart(int id, Cart cart)
+        {
+            if (id != cart.OrderID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cart).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Carts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cart>> PostCart(Cart cart)
+        {
+            try
+            {
+                cart.OrderStatus = nameof(OrderStatus.INITIATED);
+                _context.Carts.Add(cart);
+                await _context.SaveChangesAsync();
+
+                Order order = new()
+                {
+                    CustomerID = cart.CustomerID,
+                    CartID = cart.CartID
+                };
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+
+                foreach (var product in cart.Products)
+                {
+                    var orderItem = new OrderItem
+                    {
+                        OrderID = order.OrderID,
+                        ProductID = product.ProductID,
+                        Quantity = product.Quantity,
+                        ProductPrice = product.ProductPrice,
+                        Total = product.Total
+                    };
+
+                    _context.OrderItems.Add(orderItem);
+
+                }
+                await _context.SaveChangesAsync();
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                    Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+                };
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "orders", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    CartOrder cartOrder = new() { OrderID = cart.OrderID, CartID = cart.CartID, CustomerID = cart.CustomerID, Products = cart.Products };
+                    String jsonified = JsonConvert.SerializeObject(cartOrder);
+                    var body = Encoding.UTF8.GetBytes(jsonified);
+
+                    channel.BasicPublish(exchange: "", routingKey: "orders", basicProperties: null, body: body);
+                    Console.WriteLine(" [x] Sent {0}", cartOrder);
+                }
+
+                return CreatedAtAction("GetCart", new { id = cart.CartID }, cart);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        // DELETE: api/Carts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCart(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            _context.Carts.Remove(cart);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CartExists(int id)
+        {
+            return _context.Carts.Any(e => e.CartID == id);
+        }
+    }
+}
--- CartApi/Models/Cart.cs
+++ CartApi/Models/Cart.cs
@@ -1,14 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CartApi.Models
 {
     public class Cart
     {
+        [Key]
         public int CartID { get; set; }
-        public int CustomerID { get; set; }
-        public List<Product> Products { get; set; }
         public string OrderStatus { get; set; }
         public int OrderID { get; set; }
+
+        [NotMapped]
+        public int CustomerID { get; set; }
+        [NotMapped]
+        public List<Product> Products { get; set; }
+    }
+
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public int CartID { get; set; }
+        public int CustomerID { get; set; }
+    }
+
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public double ProductPrice { get; set; }
+        public double Total { get; set; }
     }
 
     public class Product
@@ -22,24 +47,8 @@ namespace CartApi.Models
     public class CartOrder
     {
         public int OrderID { get; set; }
+        public int CartID { get; set; }
         public int CustomerID { get; set; }
         public List<Product> Products { get; set; }
-    }
-
-    public class Order
-    {
-        public int OrderID { get; set; }
-        public int CustomerID { get; set; }
-        public string OrderStatus { get; set; }
-    }
-
-    public class OrderItem
-    {
-        public int OrderItemID { get; set; }
-        public int OrderID { get; set; }
-        public int ProductID { get; set; }
-        public int Quantity { get; set; }
-        public double ProductPrice { get; set; }
-        public double Total { get; set; }
     }
 }
